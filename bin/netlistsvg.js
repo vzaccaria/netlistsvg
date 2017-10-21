bff7661926e0fc679cad38b8fae04aec4505780c
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-10-21 15:16:21 +0200

    produce postscript files from verilog aswell (netlistsvg.js, package.json)

--- bin/netlistsvg.js
+++ bin/netlistsvg.js
@@ -3,14 +3,34 @@
 
 const prog = require("caporal");
 
-var lib = require("../lib");
-var $fs = require("mz/fs");
-var $gstd = require("get-stdin");
+let lib = require("../lib");
+let _ = require("lodash");
+let $fs = require("mz/fs");
+let { exec } = require("mz/child_process");
+let $gstd = require("get-stdin");
+
+let tmp = require("tmp-promise");
+
+let execWithString = (cmd, string) => {
+  return tmp.file().then(o => {
+    return $fs
+      .writeFile(o.path, string, "utf8")
+      .then(() => exec(cmd(o.path)))
+      .then(a => {
+        o.cleanup();
+        return a[0];
+      });
+  });
+};
 
 let main = () => {
   prog
-    .description("Produce a pdf file from a yosys json netlist")
+    .description("Produce a postscript file from a yosys netlist")
     .argument("[file]", "optional JSON file, otherwise read from stdin")
+    .option(
+      "--verilog",
+      "interpret [file] as a verilog file to be synth. into GTECH"
+    )
     .option(
       "--skin <filename>",
       "Use skin <filename>",
@@ -19,14 +39,32 @@ let main = () => {
     )
     .action((args, options) => {
       let skin_data = $fs.readFile(options.skin, "utf-8");
-      let netlist_data = args.file ? $fs.readFile(args.file, "utf-8") : $gstd();
+      let netlist_data;
+      if (options.verilog) {
+        if (!args.file) {
+          netlist_data = $gstd().then(filed =>
+            execWithString(
+              path => `yosys -q -o /dev/stdout -b json -f verilog -S ${path}`,
+              filed
+            ));
+        } else {
+          netlist_data = exec(
+            `yosys -q -o /dev/stdout -b json -S ${args.file}`
+          ).then(a => a[0]);
+        }
+      } else {
+        netlist_data = args.file ? $fs.readFile(args.file, "utf-8") : $gstd();
+      }
       Promise.all([skin_data, netlist_data])
         .then(([sd, nd]) => {
           nd = JSON.parse(nd);
           return lib.render(sd, nd);
         })
         .then(svg => {
-          console.log(svg);
+          return execWithString(path => `cairosvg ${path} -f ps`, svg);
+        })
+        .then(a => {
+          console.log(a);
         });
     });
   prog.parse(process.argv);
