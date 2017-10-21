4bd4423f81f5f1a6bf81e621b4e653c75fc1e484
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-10-21 12:32:34 +0200

    add caporal and exploit stdout (netlistsvg.js, index.js, package.json)

--- bin/netlistsvg.js
+++ bin/netlistsvg.js
@@ -1,11 +1,39 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-var lib = require('../lib'),
-    yargs = require('yargs');
+const prog = require("caporal");
 
-var argv = yargs
-            .demand(1)
-            .usage('usage: $0 input_json_file [-o output_svg_file] [--skin skin_file]')
-            .argv;
-lib.render(argv._[0], argv.o, argv.skin);
+var lib = require("../lib");
+var $fs = require("mz/fs");
+var $gstd = require("get-stdin");
+
+let main = () => {
+  prog
+    .description("Produce a pdf file from a yosys json netlist")
+    .argument("[file]", "optional JSON file, otherwise read from stdin")
+    .option(
+      "--skin <filename>",
+      "Use skin <filename>",
+      prog.STRING,
+      `${__dirname}/../lib/default.svg`
+    )
+    .option(
+      "--output <file>",
+      "optional PDF filename, otherwise print to stdout"
+    )
+    .action((args, options) => {
+      let skin_data = $fs.readFile(options.skin, "utf-8");
+      let netlist_data = args.file ? $fs.readFile(args.file, "utf-8") : $gstd();
+      Promise.all([skin_data, netlist_data])
+        .then(([sd, nd]) => {
+          nd = JSON.parse(nd);
+          return lib.render(sd, nd);
+        })
+        .then(svg => {
+          console.log(svg);
+        });
+    });
+  prog.parse(process.argv);
+};
+
+main();
