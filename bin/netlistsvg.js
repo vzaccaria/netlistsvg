0a96c7ae40a3c340818c2d45f5b0ed764a1562dd
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-10-22 18:07:53 +0200

    add waveform generation from wavedrom (netlistsvg.js, package.json)

--- bin/netlistsvg.js
+++ bin/netlistsvg.js
@@ -8,16 +8,25 @@ let _ = require("lodash");
 let $fs = require("mz/fs");
 let { exec } = require("mz/child_process");
 let $gstd = require("get-stdin");
+let debug = require("debug")("netsvg");
 
 let tmp = require("tmp-promise");
 
-let execWithString = (cmd, string) => {
-  return tmp.file().then(o => {
+let execWithString = (cmd, string, options) => {
+  let keep = !_.get(options, "cleanup", true);
+  let postfix = _.get(options, "postfix", ".tmp");
+  return tmp.file({ postfix, keep }).then(o => {
     return $fs
       .writeFile(o.path, string, "utf8")
-      .then(() => exec(cmd(o.path)))
+      .then(() => {
+        let cc = cmd(o.path);
+        debug(cc);
+        return exec(cc);
+      })
       .then(a => {
-        o.cleanup();
+        if (!keep) {
+          o.cleanup();
+        }
         return a[0];
       });
   });
@@ -25,7 +34,8 @@ let execWithString = (cmd, string) => {
 
 let main = () => {
   prog
-    .description("Produce a postscript file from a yosys netlist")
+    .description("Produce either a netlist diagram or a wave diagram")
+    .command("netlist", "produce a netlist diagram")
     .argument("[file]", "source file (or stdin). Can be verilog or JSON")
     .option(
       "--verilog",
@@ -47,7 +57,8 @@ let main = () => {
             execWithString(
               path => `yosys -q -o /dev/stdout -b json -f verilog -S ${path}`,
               filed
-            ));
+            )
+          );
         } else {
           netlist_data = exec(
             `yosys -q -o /dev/stdout -b json -S ${args.file}`
@@ -71,6 +82,25 @@ let main = () => {
         .then(a => {
           console.log(a);
         });
+    })
+    .command("wave", "produce a wave diagram")
+    .argument("[file]", "JSON file")
+    .action((args, options) => {
+      let wave_data = args.file ? $fs.readFile(args.file, "utf-8") : $gstd();
+      wave_data.then(file => {
+        return execWithString(
+          path =>
+            `phantomjs ${__dirname}/../node_modules/wavedrom-cli/bin/wavedrom-cli.js -i ${path} -s /dev/stdout`,
+          file,
+          { postfix: ".js", cleanup: false }
+        )
+          .then(svg => {
+            return execWithString(path => `cairosvg ${path} -f ps`, svg);
+          })
+          .then(a => {
+            console.log(a);
+          });
+      });
     });
   prog.parse(process.argv);
 };
---
+++ examples/singleAdder.v
@@ -0,0 +1,9 @@
+module singleAdder(
+   input  a,
+   input  b,
+   input  cin,
+   output s,
+   output cout );
+   assign s = a ^ b ^ cin;
+   assign cout = (a & b)  |  (a & cin)  |  (b & cin);
+endmodule
