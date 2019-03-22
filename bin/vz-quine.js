4a722bdd979f863c31431a1835532298b6f40447
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2019-03-22 17:02:42 +0100

    update: produce bitmasks (vz-quine.js)

--- bin/vz-quine.js
+++ bin/vz-quine.js
@@ -13,24 +13,51 @@ let tt2qm = (n, tt) => {
   data.compute();
   let returned = {};
 
+  let valueWithMask = (bits, mask) => {
+    let str = [];
+    var res = bits.toString(2);
+    for (var j = 0; j < n; j++) {
+      var currentBit = Math.pow(2, n - 1 - j);
+      if ((currentBit & mask) === currentBit) {
+        str.push("-");
+      } else {
+        if (j >= n - res.length) {
+          str.push(res.charAt(j - (n - res.length)));
+        } else {
+          str.push("0");
+        }
+      }
+    }
+    return str;
+  };
+
+  let implicantString = ({ values, mask }) => {
+    let val = _.reduce(values, (a, v) => a | v, 0);
+    return valueWithMask(val, mask);
+  };
+
   let getImplicantGroup = g => {
     return _.map(g.group, i => {
-      return {
+      let im = {
         values: _.map(i.imp),
         essential: i.used,
         prime: i.isPrim,
         mask: i.bitMask
       };
+      im.string = implicantString(im);
+      return im;
     });
   };
 
   returned.primeImplicants = _.map(data.primTerms, i => {
-    return {
+    let im = {
       values: _.map(i.implicant.imp),
       essential: i.used,
       prime: i.isPrim,
       mask: i.bitMask
     };
+    im.string = implicantString(im);
+    return im;
   });
   returned.implicantsTables = _.map(data.implicantGroups, getImplicantGroup);
 
