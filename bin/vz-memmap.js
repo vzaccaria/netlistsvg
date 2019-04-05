77d939c35209c25b9861c2dc05dd5166e1f2080b
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2019-04-05 12:59:28 +0200

    update: add memmap generation (package.json)

---
+++ bin/vz-memmap.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+"use strict";
+
+let _ = require("lodash");
+let $fs = require("mz/fs");
+let $gstd = require("get-stdin");
+let { execWithString } = require("./common");
+let { exec } = require("mz/child_process");
+
+const prog = require("caporal");
+
+let main = () => {
+  prog
+    .description("Memory map generator")
+    .command("generate")
+    .argument("[csvfile]>")
+    .action((args, options, logger) => {
+      (args.csvfile ? $fs.readFile(args.csvfile, "utf8") : $gstd()).then(data =>
+        execWithString(
+          path => `${__dirname}/generate_mem_diagram.py -i ${path}`,
+          data,
+          { logger }
+        ).then(output => {
+          console.log(output);
+        })
+      );
+    })
+    .command("preamble")
+    .action(() => {
+      exec(`${__dirname}/generate_mem_diagram.py -r`).then(output => {
+        console.log(output[0]);
+      });
+    });
+  prog.parse(process.argv);
+};
+
+main();
