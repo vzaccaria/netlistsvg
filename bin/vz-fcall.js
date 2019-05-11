64af209e8d6e7736d55f6cd1aa4847a685921534
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2019-05-11 12:39:55 +0200

    update: streamline creation of arrays and introduce default call in spec (mipsfcall.json, mipsfcall1.json, vz-fcall.js)

--- bin/vz-fcall.js
+++ bin/vz-fcall.js
@@ -8,12 +8,13 @@ const _ = require("lodash");
 
 let $fs = require("mz/fs");
 
-let vrre = /(?<name>[\w\[\.\]]+)\/((?<register>\$[\w]+)|(?<size>[\d]+))/;
+let vrre = /(?<name>[\w\[\.\]]+)\/((?<register>\$[\w]+)|(?<size>[\d]+))(?<arrayof>[bw])?/;
 
 let _getvarreg = s => {
   let gg = s.match(vrre).groups;
   if (_.isUndefined(gg.size)) gg.size = 4;
   else gg.size = parseInt(gg.size);
+  if (gg.arrayof === "w") gg.size = gg.size * 4;
   return gg;
 };
 
@@ -23,7 +24,7 @@ let wrap = (data, c) => {
 zone/.style={on chain=1, dashed, draw=gray, text centered, text width=3cm, minimum height = 30mm, text=gray},
 parameter/.style={on chain=1, dashed, draw=gray, text centered, text width=3cm, minimum height = 10mm, text=gray},
 savedreg/.style={on chain=1, draw=black, text centered, text width=3cm, minimum height = 10mm},
-localvar/.style={on chain=1, draw=black, text centered, text width=3cm, minimum height = 10mm, fill=blue!30}]
+localvar/.style={on chain=1, draw=black, text centered, text width=3cm, minimum height = 10mm, fill=blue!10}]
 ${c}
 \\end{tikzpicture}
 `;
@@ -43,13 +44,52 @@ let gpdiag = (data, cells) => {
         lab = `,label=right:{${i.offset}}`;
       }
       if (!_.isUndefined(i.pointedBy)) {
-        lab = `${lab},label=left:${_n(i.pointedBy)}`;
+        lab = `${lab},label=left:${_n(i.pointedBy)}$\\rightarrow$`;
       }
       return `\\node [${i.type}${lab}] {${_n(i.name)} ${cause}};`;
     }),
     "\n"
   );
   return wrap(data, txt);
+};
+
+let unfoldVariable = (cells, { size, name, arrayof, type }) => {
+  if (arrayof === "w") {
+    let as = _.range(0, size, 4);
+    let rgs = _.map(as, (o, i) => {
+      return {
+        type,
+        name: `${name}[${i}]`,
+        size: 4,
+        base: i === 0
+      };
+    });
+    _.map(_.reverse(rgs), r => cells.push(r));
+  } else {
+    if (arrayof === "b") {
+      let as = _.range(0, size, 4);
+      let rgs = _.reverse(
+        _.map(as, (v, i) => {
+          return { lb: v, ub: Math.min(v + 3, size - 1), base: i === 0 };
+        })
+      );
+      _.map(rgs, ({ ub, lb, base }) => {
+        cells.push({
+          type,
+          name: `${name}[${ub}..${lb}]`,
+          size: 4,
+          base
+        });
+      });
+    } else {
+      cells.push({
+        type,
+        name,
+        size,
+        base: true
+      });
+    }
+  }
 };
 
 let gpstate = (data, { callee, caller }) => {
@@ -62,9 +102,9 @@ let gpstate = (data, { callee, caller }) => {
   cells.push({ type: "zone", name: caller, size: 0, cause: "previous frame" });
 
   _.map(cee.parameters, l => {
-    let { name, register, size } = _getvarreg(l);
+    let { name, register, size, arrayof } = _getvarreg(l);
     if (!register) {
-      cells.push({ type: "parameter", name: name, size: size });
+      unfoldVariable(cells, { name, size, arrayof, type: "parameter" });
     }
   });
 
@@ -84,7 +124,7 @@ let gpstate = (data, { callee, caller }) => {
   }
 
   _.map(cee.localvars, l => {
-    let { name, register, size } = _getvarreg(l);
+    let { name, register, size, arrayof } = _getvarreg(l);
     if (register) {
       cells.push({
         type: "savedreg",
@@ -93,11 +133,7 @@ let gpstate = (data, { callee, caller }) => {
         size: 4
       });
     } else {
-      cells.push({
-        type: "localvar",
-        name,
-        size
-      });
+      unfoldVariable(cells, { name, size, arrayof, type: "localvar" });
     }
   });
   cells[cells.length - 1].pointedBy = "$sp";
@@ -117,12 +153,16 @@ let main = () => {
     .description("Function call utils")
     .command("prologue", "generates stack and register usage for a call")
     .argument("<json>", `File describing the call sequence`)
-    .argument("<invocation>", `Example: 'f,g' means f invokes g`)
-    .action(async args => {
+    .option("-j, --json", `print sequence of cells instead of tikz diagram`)
+    .action(async (args, options) => {
       let data = await (args.json ? $fs.readFile(args.json, "utf8") : $gstd());
       data = JSON.parse(data);
-      let [caller, callee] = args.invocation.split(",");
-      console.log(gpdiag(data, gpstate(data, { caller, callee })));
+      let r = gpstate(data, data.defaultCall);
+      if (options.json) {
+        console.log(r);
+      } else {
+        console.log(gpdiag(data, r));
+      }
     });
   prog.parse(process.argv);
 };
