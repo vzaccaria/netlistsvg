98f1dc7067c72819865082c6e5bdf5abe4c9d5cb
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2019-10-12 18:44:31 +0200

    update: minor changes (rv-fatt.s)

--- bin/fixtures/rv-fatt.s
+++ bin/fixtures/rv-fatt.s
@@ -1,10 +1,10 @@
-        mv s0, a0
-        li t0, 1 
-        blt s0, t0, exit1
-        addi t0, s0, -1
-        mv a0, t0
-        call fatt
-        mul a0, a0, s0
+        mv s0, a0           # savedN = n
+        li t0, 1            
+        blt s0, t0, exit1   # savedN < 1 goto exit1
+        addi t0, s0, -1     # tmp = savedN - 1
+        mv a0, t0            
+        call fatt           # fatt(tmp)
+        mul a0, a0, s0      # a0 = savedN * fatt(tmp)
         j fattEPI
 
 exit1: 
